5161886bef891dd249a3d683e5fe2cb837b13cee
Giovan Dias <45858575+gio-dev@users.noreply.github.com>
2025-03-10 07:52:17 -0300

    Corrções

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,8 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
-import tailwind from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
 export default defineConfig({
     plugins: [
-        postcss({
-            plugins: [tailwind(), autoprefixer()],
-        }),
         laravel({
             input: 'resources/js/app.ts',
             refresh: true,
